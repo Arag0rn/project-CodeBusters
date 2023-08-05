013cd444a4806d3d8eb9ac6f3cb1d3a499c6a29b
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-08-05 14:21:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './js/categories';
 import './js/support-ukraine';
 import './js/book-by-category';
 import './js/best-sellers';
-import { close, modal, onSignOnclick, onCloseClick } from './js/auth-modal.js';
+import './js/auth-modal.js';
 
