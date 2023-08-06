49c409e141d56b902906d80424a2f487819d0fe0
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-08-06 13:45:36 +0300

    change

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -5,52 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const hiddenItems = document.querySelectorAll(".hidden");
     const elems = document.querySelectorAll(".elem");
     const container = document.querySelector(".support-ukraine-container");
+    const block = document.querySelector(".support-ukraine-block");
     let isExpanded = false;
 
-    showMoreButton.addEventListener("click", function () {
-      if (!isExpanded) {
 
-        hiddenItems.forEach(function (item) {
-          item.style.display = "list-item";
-        });
-
-          showMoreButton.style.display = "none";
-          showMoreButtonSecond.style.display = "block";
-
-          elems.forEach(function (item) {
-          item.style.display = "none";
-        });
-
-
-        const lastHiddenItem = hiddenItems[hiddenItems.length - 1];
-        lastHiddenItem.scrollIntoView({ behavior: "smooth" });
-        fourth.style.marginTop = "0px"
-        isExpanded = true;
-      }
-    });
-
-    showMoreButtonSecond.addEventListener("click", function () {
-    if (isExpanded) {
-
-      hiddenItems.forEach(function (item) {
-        item.style.display = "none";
-      });
-
-
-        showMoreButtonSecond.style.display = "none";
-        showMoreButton.style.display = "block";
-
-
-      elems.forEach(function (item, index) {
-        if (index < 6) {
-            item.style.display = "list-item";
-            item.style.display = "flex";
-        }
-      });
-
-      container.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest"  });
-       fourth.style.marginTop = "20px";
-      isExpanded = false;
-    }
+  showMoreButton.addEventListener('click', function () {
+    block.classList.toggle('expanded');
   });
-  });
+});
