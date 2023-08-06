466a13610004484694a2683f4ac8cdc70f0e704d
Pangpang <vicleeua@gmail.com>
2023-08-06 18:04:31 +0900

    mob menu/logo link

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,17 +1,25 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
 (() => {
   const mobileMenu = document.querySelector('.js-mobile-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const mobMenuBox = document.querySelector('.js-mob-menu');
   const closeMenuBtn = document.querySelector('.js-menu-close');
+  const header = document.querySelector('.header-container');
+
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true';
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
     mobMenuBox.classList.toggle('is-open');
-    const scrollLockMethod = isMenuOpen
-      ? 'enableBodyScroll'
-      : 'disableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+
+    if (isMenuOpen) {
+      enableBodyScroll(document.body);
+      enableBodyScroll(header);
+    } else {
+      disableBodyScroll(document.body);
+      disableBodyScroll(header);
+    }
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
