d7e5c129be6264024f0fd655b72d7c2c7f7f3781
Olena <petrova1512@gmail.com>
2023-08-06 16:56:26 +0200

    make logic category select

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import './js/mobile-menu';
-import "./js/main-shop-list"
-import './js/theme-switcher';
 import './js/categories';
+import './js/best-sellers';
+import './js/mobile-menu';
+import './js/main-shop-list';
+import './js/theme-switcher';
 import './js/support-ukraine';
 import './js/book-by-category';
-import './js/best-sellers';
 import './js/auth-modal.js';
 import './js/auth.js';
 import './js/user-bar';
 import './js/scrol-to-top.js';
-
