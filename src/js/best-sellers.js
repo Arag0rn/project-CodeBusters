dad2e7d05a7a8d12254f49b7db0d8e74a6f02e44
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-08-07 18:55:07 +0300

    clear code

--- src/js/best-sellers.js
+++ src/js/best-sellers.js
@@ -1,29 +1,3 @@
-// import axios from "axios";
-// async function loadCategories() {
-//   const response = await axios.get("https://books-backend.p.goit.global/books/category-list");
-//   return response.data;
-// }
-
-
-
-
-
-// async function renderCategories() {
-//   try{
-//     const categories = await loadCategories();
-//     const markup = categories.map(({list_name})=> {return ``})
-//   }
-// }
-// renderCategories();
-
-
-// async function fetchbooks() {
-//   const response = await axios.get()
-// }
-
-
-
-
 import axios from "axios";
 
 async function loadCategoriesAndBooks() {
